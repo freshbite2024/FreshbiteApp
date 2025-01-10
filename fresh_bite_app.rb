f87c4d268178c014a3427b6d107fde0edd1cbaf3
Munendra <freshbite.india@gmail.com>
2025-01-10 13:16:30 +0530

    code optimized

--- fresh_bite_app.rb
+++ fresh_bite_app.rb
@@ -1,65 +1,35 @@
 require 'sinatra'
 require 'active_record'
 require 'mysql2'
-require 'dotenv/load' # Optional for .env file
+require 'dotenv/load'  # Optional for .env file
 require 'sinatra/namespace'
 
-# Explicitly establishing ActiveRecord connection with SSL disabled
-ActiveRecord::Base.establish_connection(
-  adapter: 'mysql2',
-  host: 'mysql-freshbite-munendra0412-3e77.c.aivencloud.com',
-  port: '27005',
-  database: 'freshbite',
-  username: 'munendra',
-  password: 'AVNS_AtTD6atRjgWRyaDKs7q',
-  ssl_mode: 'DISABLED'  # Directly disable SSL
-)
+# Load helpers dynamically
+Dir["helpers/*.rb"].each { |helper| require_relative helper }
 
-# Example Model
-class User < ActiveRecord::Base
-end
-
-helpers = Dir.glob("helpers/*.rb")
-helpers = helpers.flatten.uniq
-helpers.each do |helper|
-  require_relative "#{helper}"
-end
-
-routes = Dir.glob("routes/*.rb")
-routes = routes.flatten.uniq
-routes.each do |route|
-  require_relative "#{route}"
-end
+# Load routes dynamically
+Dir["routes/*.rb"].each { |route| require_relative route }
 
 class FreshBiteApp < Sinatra::Base
   set :root, File.dirname(__FILE__)
-  register Sinatra::Namespace
-  
-  set :public_folder, 'views' # Serve static files from the views folder
+  set :public_folder, 'views'  # Serve static files from the views folder
 
+  register Sinatra::Namespace
+  register Sinatra::ActiveRecordExtension
+
+  # Default route for the main page
   get '/' do
     send_file File.join(settings.public_folder, 'index.html')
   end
-  namespace "/api" do
-    get "/" do
-      "Hello world!, Welcome to RetailerAppStg API. This is V1 Application."
+
+  # API namespace and routes
+  namespace '/api' do
+    get '/' do
+      'Hello world! Welcome to RetailerAppStg API. This is V1 Application.'
     end
 
-    # Test MySQL connection
-    get '/test_connection' do
-      # begin
-        # Check if ActiveRecord connection is active
-        if ActiveRecord::Base.connection.active?
-          "MySQL connection is successful!"
-        else
-          "MySQL connection is not active!"
-        end
-      # rescue => e
-      #   "MySQL connection failed: #{e.message}"
-      # end
-    end
-
-    namespace "/index" do
+    # Load additional routes under /api/index namespace
+    namespace '/index' do
       register Sinatra::RetailerApp::Routing::SampleRoutes
     end
   end
